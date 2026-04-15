8c09ee797234114e1894425eb87909d22e72fe09
sara luongo <saraluongo202002@gmail.com>
2026-04-15 18:08:19 +0200

    eseguita logica js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
+const contenitoreEmail = 'https://flynn.boolean.careers/exercises/api/random/mail';
+const generatoreMail = document.querySelector('#mail');
 
+
+for (let i = 0; i < 10; i++) {
+    fetch(contenitoreEmail)
+        .then((risposta) => {
+            return risposta.json()
+        })
+        .then((dato) => {
+            const randomMail = dato.response
+            generatoreMail.innerHTML += `<li>${randomMail}</li>`
+        })
+}
